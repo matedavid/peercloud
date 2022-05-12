2abf276b865ce1b3c75a2f1e67283051f48b6cc2
matedavid <david.mate.lopez@gmail.com>
2022-05-12 16:46:54 +0200

    uing NetworkCommand enum as value in MessageHeader Struct and added more network commands

--- network/header.go
+++ network/header.go
@@ -3,9 +3,7 @@ package network
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 	"net"
-	"strings"
 )
 
 // Network codes
@@ -18,46 +16,48 @@ const (
 	Version NetworkCommand = iota
 	Verack
 	Store
+	Stored
 	Retrieve
+	Acknowledge
 	Unknown
 )
 
-func Command2Bytes(command NetworkCommand) [12]byte {
-	var byteCommand [12]byte
+// func Command2Bytes(command NetworkCommand) [12]byte {
+// 	var byteCommand [12]byte
 
-	switch command {
-	case Version:
-		byteCommand = [12]byte{'v', 'e', 'r', 's', 'i', 'o', 'n'}
-	case Verack:
-		byteCommand = [12]byte{'v', 'e', 'r', 'a', 'c', 'k'}
-	case Store:
-		byteCommand = [12]byte{'s', 't', 'o', 'r', 'e'}
-	case Retrieve:
-		byteCommand = [12]byte{'r', 'e', 't', 'r', 'i', 'e', 'v', 'e'}
-	}
+// 	switch command {
+// 	case Version:
+// 		byteCommand = [12]byte{'v', 'e', 'r', 's', 'i', 'o', 'n'}
+// 	case Verack:
+// 		byteCommand = [12]byte{'v', 'e', 'r', 'a', 'c', 'k'}
+// 	case Store:
+// 		byteCommand = [12]byte{'s', 't', 'o', 'r', 'e'}
+// 	case Retrieve:
+// 		byteCommand = [12]byte{'r', 'e', 't', 'r', 'i', 'e', 'v', 'e'}
+// 	}
 
-	return byteCommand
-}
+// 	return byteCommand
+// }
 
-func Bytes2Command(byteCommand [12]byte) NetworkCommand {
-	strCommand := string(byteCommand[:])
-	strCommand = strings.Trim(strCommand, "\x00")
+// func Bytes2Command(byteCommand [12]byte) NetworkCommand {
+// 	strCommand := string(byteCommand[:])
+// 	strCommand = strings.Trim(strCommand, "\x00")
 
-	if strCommand == "version" {
-		return Version
-	} else if strCommand == "verack" {
-		return Verack
-	} else if strCommand == "store" {
-		return Store
-	} else if strCommand == "retrieve" {
-		return Retrieve
-	}
-	return Unknown
-}
+// 	if strCommand == "version" {
+// 		return Version
+// 	} else if strCommand == "verack" {
+// 		return Verack
+// 	} else if strCommand == "store" {
+// 		return Store
+// 	} else if strCommand == "retrieve" {
+// 		return Retrieve
+// 	}
+// 	return Unknown
+// }
 
 type MessageHeader struct {
 	NetworkCode uint32
-	Command     [12]byte
+	Command     NetworkCommand
 	Payload     uint32
 }
 
@@ -70,8 +70,7 @@ func (mh *MessageHeader) Send(conn net.Conn) error {
 	}
 
 	// Send to other node
-	n, err := conn.Write(payload.Bytes())
-	fmt.Println("Sent:", n, "bytes")
+	_, err = conn.Write(payload.Bytes())
 	return err
 }
 
