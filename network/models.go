9a79a5073fa47ffda4849c50f127948f20ba3aca
matedavid <david.mate.lopez@gmail.com>
2022-06-06 03:05:16 +0200

    VersionPayload with Write/Read methods

--- network/models.go
+++ network/models.go
@@ -1,5 +1,13 @@
 package network
 
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+	"strconv"
+	"strings"
+)
+
 type Model interface {
 	// Convert Model to []byte
 	Write() []byte
@@ -18,6 +26,13 @@ type UploadPayload struct {
 
 type DownloadPayload struct {
 	Hash string
+}
+
+type VersionPayload struct {
+	Timestamp  int64
+	Address    net.IP
+	Port       uint32
+	Identifier string
 }
 
 // GenericPayload
@@ -46,4 +61,60 @@ func (p *DownloadPayload) Write() []byte {
 
 func (p *DownloadPayload) Read(data []byte) {
 	p.Hash = string(data[:64])
+}
+
+// VersionPayload
+func (p *VersionPayload) Write() []byte {
+	// TODO: Should treat errors somehow
+	timestampBytes := &bytes.Buffer{}
+	binary.Write(timestampBytes, binary.LittleEndian, p.Timestamp)
+
+	portBytes := &bytes.Buffer{}
+	binary.Write(portBytes, binary.LittleEndian, p.Port)
+
+	// TODO: Should create independent function to do this (net.IP => []byte)
+	addressBytes := make([]byte, 4)
+	bytesStr := strings.Split(p.Address.String(), ".")
+	for i, b := range bytesStr {
+		intVal, _ := strconv.Atoi(b)
+
+		bBytes := &bytes.Buffer{}
+		binary.Write(bBytes, binary.LittleEndian, uint8(intVal))
+		addressBytes[i] = bBytes.Bytes()[0]
+	}
+
+	var data []byte
+	data = append(data, timestampBytes.Bytes()...)
+	data = append(data, addressBytes...)
+	data = append(data, portBytes.Bytes()...)
+	data = append(data, []byte(p.Identifier)...)
+
+	return data
+}
+
+func (p *VersionPayload) Read(data []byte) {
+	timestampBytes := data[:8]
+	addressBytes := data[8:12]
+	portBytes := data[12:16]
+	identifierBytes := data[16:]
+
+	// TODO: Should treat errors somehow
+	binary.Read(bytes.NewReader(timestampBytes), binary.LittleEndian, &p.Timestamp)
+	binary.Read(bytes.NewReader(portBytes), binary.LittleEndian, &p.Port)
+
+	// TODO: Should create independent function to do this ([]byte => net.IP)
+	bytesStr := make([]string, 4)
+	for i, b := range addressBytes {
+		var value uint8
+
+		sliceByte := make([]byte, 1)
+		sliceByte[0] = b
+
+		binary.Read(bytes.NewReader(sliceByte), binary.LittleEndian, &value)
+		bytesStr[i] = strconv.Itoa(int(value))
+	}
+
+	addressStr := bytesStr[0] + "." + bytesStr[1] + "." + bytesStr[2] + "." + bytesStr[3]
+	p.Address = net.ParseIP(addressStr)
+	p.Identifier = string(identifierBytes)
 }
